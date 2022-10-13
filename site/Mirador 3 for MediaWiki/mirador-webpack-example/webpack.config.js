a38f9b4245d701f9044eaf960704651b9c21be88
D-Groenewegen <dennisgroenewegen@gmail.com>
2022-10-13 15:19:21 +0200

    Update webpack.config.js

--- site/Mirador 3 for MediaWiki/mirador-webpack-example/webpack.config.js
+++ site/Mirador 3 for MediaWiki/mirador-webpack-example/webpack.config.js
@@ -1,1 +1,13 @@
+const path = require('path');
 
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'auto',
+  },
+  stats: {
+    errorDetails: true,
+  },
+};
