5f90e34031db2692105040112cc742d8e441282d
D-Groenewegen <dennisgroenewegen@gmail.com>
2022-10-13 15:20:47 +0200

    Update index.js

--- site/Mirador 3 for MediaWiki/mirador-webpack-example/src/index.js
+++ site/Mirador 3 for MediaWiki/mirador-webpack-example/src/index.js
@@ -1,1 +1,30 @@
+import Mirador from 'mirador/dist/es/src/index';
+import { miradorImageToolsPlugin } from 'mirador-image-tools';
+import imageCropperPlugin from 'mirador-imagecropper/es';
 
+const getManifest = document.getElementById('miradorframe').getAttribute('data-manifest-url');
+
+const config = {
+  id: 'miradorframe',
+  windows: [{
+    imageToolsEnabled: true,
+    imageToolsOpen: true,
+    imageCropper: {
+      active: true,
+      enabled: true
+    },
+    manifestId: getManifest,
+  }],
+  theme: {
+    palette: {
+      primary: {
+        main: '#1967d2',
+      },
+    },
+  },
+};
+
+Mirador.viewer(config, [
+  ...miradorImageToolsPlugin,
+  ...imageCropperPlugin
+]);
